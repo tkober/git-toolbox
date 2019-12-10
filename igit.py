be60c1a5cbb23e1d8e10637d3d9cdf4f0ffeed4e
Thorsten Kober <thorsten.kober@softgarden.de>
2019-12-10 22:22:26 +0100

    Set up project

---
+++ igit.py
@@ -0,0 +1,19 @@
+import curses
+
+def main(stdscr):
+    # turn off cursor blinking
+    curses.curs_set(0)
+
+    # color scheme for selected row
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+
+    # specify the current selected row
+    current_row = 0
+
+    # print the menu
+
+    while 1:
+        stdscr.clear()
+        stdscr.refresh()
+
+curses.wrapper(main)
