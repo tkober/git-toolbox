7acc47bd7980d1e891c8297ff33ca56274d7000e
Thorsten Kober <thorsten.kober@softgarden.de>
2019-12-14 00:54:14 +0100

    Built install and run scripts

--- run.sh
+++ run.sh
@@ -1,1 +1,15 @@
 #!/usr/bin/env bash
+
+ENV_NAME="git-stage-env"
+
+if [ ! -z "$1" ]
+  then
+    cd $1
+fi
+
+if [ -x "$(command -v conda)" ]; then
+  source activate $ENV_NAME
+fi
+
+BASEDIR=$(dirname "$0")
+python "$BASEDIR/app.py"
