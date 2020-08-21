a7b8978969b0b80b69a70425a1a938593be0297d
Thorsten Kober <thorsten.kober@softgarden.de>
2020-08-21 15:53:14 +0200

    prototyping of branches

---
+++ branches.py
@@ -0,0 +1,25 @@
+import argparse
+import os
+from utils.git import Repository
+
+class UI():
+
+    def __init__(self, branches):
+        self.__branches = branches
+
+
+def parseArguments():
+    argparser = argparse.ArgumentParser(
+        prog='commit',
+        description='Gives you an interactive overview of all branches'
+    )
+    return argparser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parseArguments()
+
+    repositoryDirectory = os.getcwd()
+    repo = Repository(repositoryDirectory)
+
+    print(repo.getBranches())
