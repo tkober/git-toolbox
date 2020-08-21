16bd0ea78e5663ae6e52a2a3ae117e9a180d3e09
Thorsten Kober <thorsten.kober@softgarden.de>
2020-08-21 13:38:10 +0200

    Added push flag

--- commit.py
+++ commit.py
@@ -9,15 +9,27 @@ def parseArguments():
         description='Runs git-commit with commit message that is prefilled with the ticket number.'
     )
     argparser.add_argument(
-        '-s',
-        '--simple',
-        help="Commit message will not be prefilled",
+        '-e',
+        '--empty',
+        help="Commit message will not be prefilled.",
+        action="store_true"
+    )
+    argparser.add_argument(
+        '-p',
+        '--push',
+        help="After successful commit the current branch is immediately pushed to the default remote.",
+        action="store_true"
+    )
+    argparser.add_argument(
+        '-n',
+        '--no-verify',
+        help="Verify hooks will be bypassed. This effects both commit and possible push hooks.",
         action="store_true"
     )
     args = argparser.parse_args()
     return args
 
-def commit(content=''):
+def commit(content='', noVerify=False):
     tmpFilePath = os.path.expanduser('~/.tmp_commit_template')
     tmpFile = open(tmpFilePath, 'w')
     tmpFile.write(content)
@@ -30,12 +42,21 @@ def commit(content=''):
 
     return returnCode == 0
 
+def push(noVerify=False):
+    command = ['git', 'push']
+    if noVerify:
+        command.append('--no-verify')
+
+    returnCode = call(command)
+
+    return returnCode == 0
+
 if __name__ == '__main__':
     args = parseArguments()
 
     commitSuccessful = False
-    if args.simple:
-        commitSuccessful = commit()
+    if args.empty:
+        commitSuccessful = commit(noVerify=args.no_verify)
 
     else:
         repository_directory = os.getcwd()
@@ -45,12 +66,10 @@ if __name__ == '__main__':
         ticket = '-'.join(name.split('-')[:2])
 
         if ticket in ['master', 'develop']:
-            commitSuccessful = commit()
+            commitSuccessful = commit(noVerify=args.no_verify)
         else:
-            commitSuccessful = commit('{}'.format(ticket))
+            commitSuccessful = commit('{}'.format(ticket), args.no_verify)
 
 
-    if commitSuccessful:
-        print('okay')
-    else:
-        print('nooo')
+    if commitSuccessful and args.push:
+        push(args.no_verify)
